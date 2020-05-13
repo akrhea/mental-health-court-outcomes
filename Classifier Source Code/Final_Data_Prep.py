1491f249507fff40c6d69e5de21b3d8bba9fcc11
alene <alene.rhea@gmail.com>
2020-05-13 00:05:50 -0400

    organizing repo

---
+++ Classifier Source Code/Final_Data_Prep.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+"""
+Created on Sat Jan 25 16:08:54 2020
+
+@author: kelseymarkey
+edited by akrhea
+"""
+
+def remove_final_dummy(df):
+    '''
+    Remove final dummy variable of each categorical feature.
+    Changes df in-place.
+
+    Assumes df has been 'exploded' by one-hot encoding.
+    '''
+
+    #read in pre-exploded non-OHE df, with data still in categorical format
+    cat_df = pd.read_pickle('init_clean.pckl.gz', compression = 'gzip')
+
+    #list of all categorical columns to get dummies for
+    #excluding ID numbers, numerical features, datetime features, binary features
+    cat_cols = ['offense_category', 'charge_offense_title', 'chapter', 'act', 
+                'section', 'class', 'aoic', 'event', 'gender', 'race', 
+                'law_enforcement_agency', 'unit', 'incident_city', 'updated_offense_category']
+    for col in cat_cols:
+        final_value = col+"_"+cat_df[col].unique()[-1]
+        if final_value in df.columns:
+            df.drop(columns=final_value, inplace=True)
+    return
+ 
+
+def get_train_val_test (df, train_size, test_size, val_size): 
+    '''
+    Uses "timeline" method to split Data into Train / Validate / Test Sets
+    Returns a static validation set with divisions based on timeline to reduce 
+    leakage and ensure that we only ever predict forward in time. Potential danger 
+    of bias because training set will have higher proportion of "finalized" rows.
+
+    Takes in df (assumes target variable = 'MHI') and train, test, and val sizes (fractions of 1)
+    Returns xtrain, xval, xtest, ytrain, yval, ytest, with received_date dropped.
+    '''
+
+
+    #check that specificed sizes sum to 1
+    assert train_size + test_size + val_size == 1, 'train_size + test_size + val_size must sum to 1'
+
+    #sort ascending by received date
+    sorted_df = df.sort_values(axis = 0, by=['received_date'])
+    sorted_df = sorted_df.drop(columns = 'received_date')
+
+    #set x and y
+    x = sorted_df.drop('MHI', axis=1)
+    y = sorted_df['MHI']
+
+    total_size = len(x)
+
+    #first train_size % indices in train
+    xtrain = x.iloc[:int(round(train_size*total_size))]
+    ytrain = y.iloc[:int(round(train_size*total_size))]
+
+    #next val_size % indices in val
+    xval = x.iloc[int(round(train_size*total_size)):int(round((train_size+val_size)*total_size))]
+    yval = y.iloc[int(round(train_size*total_size)):int(round((train_size+val_size)*total_size))]
+
+    #final test_size % indices in test
+    xtest = x.iloc[int(round((train_size+val_size)*total_size)):-1]
+    ytest = y.iloc[int(round((train_size+val_size)*total_size)):-1]
+
+    return xtrain, xval, xtest, ytrain, yval, ytest
+
+
+def get_train_test (df, train_size, test_size): 
+    '''
+    Used for getting train/test split for training final model.
+    No validation set.
+
+    Uses "timeline" method
+
+    Takes in df (assumes 'MHI' is target variable).
+    Takes in train and test sizes (fractions of 1).
+    Returns training df and test df, with received_date dropped.
+    '''
+
+    #check that specificed sizes sum to 1
+    assert train_size + test_size == 1, 'train_size + test_size must sum to 1'
+        
+    #sort ascending by received date
+    sorted_df = df.sort_values(by=['received_date'], inplace=False)
+    sorted_df = sorted_df.drop('received_date', axis=1)
+
+    total_size = len(sorted_df)
+
+    #first train_size % indices in train
+    train = sorted_df.iloc[:int(round(train_size*total_size))]
+
+    #final test_size % indices in test
+    test = sorted_df.iloc[int(round(train_size*total_size)):-1]
+
+    return train, test
+
+
+
+'''
+Downsampling the training set
+'''
+
+def downsample(df, pct_MHI1): 
+  #takes in percentage from 1 to 50
+  #samples all MHI==1 cases
+  #samples from MHI==0 cases such that downsampled_df has pct_MHI1 % positive cases
+
+  #split into MHI 1 and MHI 0 
+  MHI1 = df[df['MHI'] == 1]
+  MHI0 = df[df['MHI'] == 0]
+
+  #count number of MHIs
+  count_MHI1 = len(MHI1)
+
+  #compute number of negative cases to sample
+  num_MHI0 = count_MHI1 * int(round((100-pct_MHI1)/pct_MHI1))
+
+  #sample from negative cases
+  MHI0_sample = MHI0.sample(n=num_MHI0, random_state=42)
+
+  #append sampled negative cases to all positive cases
+  downsampled_df = MHI1.append(MHI0_sample)
+
+  return downsampled_df
